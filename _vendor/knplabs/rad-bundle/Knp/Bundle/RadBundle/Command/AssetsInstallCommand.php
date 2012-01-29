d010b18ea1dd248f887bab2eb946b4cd36cf79c1
everzet <ever.zet@gmail.com>
2012-01-29 03:32:07 +0100

    extended assets:install to work with conventional bundles

---
+++ _vendor/knplabs/rad-bundle/Knp/Bundle/RadBundle/Command/AssetsInstallCommand.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Knp\Bundle\RadBundle\Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Finder\Finder;
+use Symfony\Bundle\FrameworkBundle\Command\AssetsInstallCommand as BaseCommand;
+
+use Knp\Bundle\RadBundle\Bundle\ConventionalBundle;
+
+class AssetsInstallCommand extends BaseCommand
+{
+    /**
+     * @see Command
+     *
+     * @throws \InvalidArgumentException When the target directory does not exist
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $targetArg = rtrim($input->getArgument('target'), '/');
+
+        if (!is_dir($targetArg)) {
+            throw new \InvalidArgumentException(sprintf('The target directory "%s" does not exist.', $input->getArgument('target')));
+        }
+
+        if (!function_exists('symlink') && $input->getOption('symlink')) {
+            throw new \InvalidArgumentException('The symlink() function is not available on your system. You need to install the assets without the --symlink option.');
+        }
+
+        $filesystem = $this->getContainer()->get('filesystem');
+
+        // Create the bundles directory otherwise symlink will fail.
+        $filesystem->mkdir($targetArg.'/bundles/', 0777);
+
+        foreach ($this->getContainer()->get('kernel')->getBundles() as $bundle) {
+            $originDir = $bundle->getPath().'/Resources/public';
+            if ($bundle instanceof ConventionalBundle) {
+                $originDir = $bundle->getPath().'/public';
+            }
+
+            if (is_dir($originDir)) {
+                $targetDir = $targetArg.'/bundles/'.preg_replace('/bundle$/', '', strtolower($bundle->getName()));
+
+                $output->writeln(sprintf('Installing assets for <comment>%s</comment> into <comment>%s</comment>', $bundle->getNamespace(), $targetDir));
+
+                $filesystem->remove($targetDir);
+
+                if ($input->getOption('symlink')) {
+                    $filesystem->symlink($originDir, $targetDir);
+                } else {
+                    $filesystem->mkdir($targetDir, 0777);
+                    // We use a custom iterator to ignore VCS files
+                    $filesystem->mirror($originDir, $targetDir, Finder::create()->in($originDir));
+                }
+            }
+        }
+    }
+}
