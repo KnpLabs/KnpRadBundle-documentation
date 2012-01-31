a2346f243983340f404f0063bb7e7797ea3e1e95
everzet <ever.zet@gmail.com>
2012-01-31 01:09:59 +0100

    fixed TemplateNameParser

--- _vendor/knplabs/rad-bundle/Knp/Bundle/RadBundle/Templating/TemplateNameParser.php
+++ _vendor/knplabs/rad-bundle/Knp/Bundle/RadBundle/Templating/TemplateNameParser.php
@@ -16,7 +16,14 @@ class TemplateNameParser extends BaseNameParser
      */
     public function parse($name)
     {
+        if ($name instanceof TemplateReferenceInterface) {
+            return $name;
+        } elseif (isset($this->cache[$name])) {
+            return $this->cache[$name];
+        }
+
         $template = parent::parse($name);
+        $name     = str_replace(':/', ':', preg_replace('#/{2,}#', '/', strtr($name, '\\', '/')));
 
         if ($template->get('bundle')) {
             if ($this->kernel->getBundle($template->get('bundle')) instanceof ConventionalBundle) {
@@ -28,6 +35,14 @@ class TemplateNameParser extends BaseNameParser
                     $template->get('engine')
                 );
             }
+        } else {
+            return $this->cache[$name] = new TemplateReference(
+                $template->get('bundle'),
+                $template->get('controller'),
+                $template->get('name'),
+                $template->get('format'),
+                $template->get('engine')
+            );
         }
 
         return $template;
