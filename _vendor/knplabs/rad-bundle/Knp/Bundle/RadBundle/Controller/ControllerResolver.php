15d2746f2ca6c6e29c19c4a69a8837864fa0787f
everzet <ever.zet@gmail.com>
2012-02-02 18:07:50 +0100

    refactored ControllerResolver

--- _vendor/knplabs/rad-bundle/Knp/Bundle/RadBundle/Controller/ControllerResolver.php
+++ _vendor/knplabs/rad-bundle/Knp/Bundle/RadBundle/Controller/ControllerResolver.php
@@ -31,14 +31,13 @@ class ControllerResolver extends BaseResolver
                 // controller in the a:b:c notation then
                 $controller = $this->parser->parse($controller);
             } elseif (1 == $count) {
-                // controller in the service:method notation
                 list($service, $method) = explode(':', $controller, 2);
 
                 if ($this->container->has($service)) {
+                    // controller in the service:method notation
                     return array($this->container->get($service), $method);
                 }
 
-                // controller in short app bundle notation
                 $controller = $this->parser->parse($controller);
             } else {
                 throw new \LogicException(sprintf('Unable to parse the controller name "%s".', $controller));
