4b89830c04e4eb35afbb2405eb7bc4ae1b429f3e
everzet <ever.zet@gmail.com>
2012-02-02 16:57:05 +0100

    removed target dir argument from assets:install as it's not needed anymore

--- _vendor/knplabs/rad-bundle/Knp/Bundle/RadBundle/Command/AssetsInstallCommand.php
+++ _vendor/knplabs/rad-bundle/Knp/Bundle/RadBundle/Command/AssetsInstallCommand.php
@@ -11,8 +11,11 @@
 
 namespace Knp\Bundle\RadBundle\Command;
 
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Output\Output;
 use Symfony\Component\Finder\Finder;
 use Symfony\Bundle\FrameworkBundle\Command\AssetsInstallCommand as BaseCommand;
 
@@ -26,9 +29,42 @@ class AssetsInstallCommand extends BaseCommand
     /**
      * {@inheritdoc}
      */
+    protected function configure()
+    {
+        $this
+            ->addOption('symlink', null, InputOption::VALUE_NONE, 'Symlinks the assets instead of copying it')
+            ->addOption('relative', null, InputOption::VALUE_NONE, 'Make relative symlinks')
+            ->setDescription('Installs bundles web assets under a public web directory')
+            ->setHelp(<<<EOT
+The <info>assets:install</info> command installs bundle assets into a web
+directory.
+
+<info>php app/console assets:install</info>
+
+A "bundles" directory will be created inside the target directory, and the
+"Resources/public" directory of each bundle will be copied into it.
+
+To create a symlink to each bundle instead of copying its assets, use the
+<info>--symlink</info> option:
+
+<info>php app/console assets:install --symlink</info>
+
+To make symlink relative, add the <info>--relative</info> option:
+
+<info>php app/console assets:install --symlink --relative</info>
+
+EOT
+            )
+            ->setName('assets:install')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $targetArg = rtrim($input->getArgument('target'), '/');
+        $targetArg = rtrim($this->getContainer()->getParameter('kernel.web_dir'), '/');
 
         if (!is_dir($targetArg)) {
             throw new \InvalidArgumentException(sprintf('The target directory "%s" does not exist.', $input->getArgument('target')));
