1f70229f2144a0155d38a03e6fc4a9c241278119
everzet <ever.zet@gmail.com>
2012-02-09 23:26:44 +0100

    moved library stuff to vendor folder

--- src/Acme/Hello/assets/js/script.js
+++ src/Acme/Hello/assets/js/script.js
@@ -1,3 +1,13 @@
+/*
+ * Require dependencies. Uses AsseticPipeline.
+ * Dependencies gets solved from src/Acme/Hello/assets & vendor/assets
+ * folders. vendor/assets is for 3rd-party styles and scripts (jquery,
+ * bootstrap):
+ *
+ *= require libs/jquery
+ *= require plugins
+ */
+
 /* Author:
 
 */
