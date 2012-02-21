636e9fb66aafc316a03646196ff639aa544af181
everzet <ever.zet@gmail.com>
2012-02-21 10:32:23 +0100

    updated kernel info

--- web/app.php
+++ web/app.php
@@ -6,5 +6,4 @@ use Knp\Bundle\RadBundle\HttpKernel\RadKernel;
 $loader = require(__DIR__.'/../vendor/.composer/autoload.php');
 $kernel = RadKernel::createAppKernel($loader, 'prod', false);
 $kernel->loadClassCache();
-//$kernel = new Knp\Bundle\RadBundle\AppCache($kernel);
 $kernel->handle(Request::createFromGlobals())->send();
